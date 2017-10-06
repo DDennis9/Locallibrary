a3c574302b688597503fa372b970d7c2bdf8af3d
dennis <meisnerdennis@web.de>
2017-10-06 13:53:18 +0200

    Tutorial Part 2

--- locallibrary/urls.py
+++ locallibrary/urls.py
@@ -13,9 +13,24 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url, include
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
+from django.views.generic import RedirectView
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
 ]
+
+# Include -> verwende urls aus Katalog-App
+urlpatterns += [
+    url(r'^catalog/', include('catalog.urls')),
+]
+
+# Umleitung zu Katalog-App, wenn r'^$' erfuellt ist.
+urlpatterns += [
+    url(r'^$', RedirectView.as_view(url='/catalog/', permanent=True))
+]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
