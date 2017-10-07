a3b6d1b34cce1fa463ce8d1c29f3809712f01707
dennis <meisnerdennis@web.de>
2017-10-07 12:47:33 +0200

    Tutorial Part 7 - login, logout, password-reset

--- locallibrary/urls.py
+++ locallibrary/urls.py
@@ -34,3 +34,7 @@ urlpatterns += [
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+urlpatterns += [
+    url(r'^accounts/', include('django.contrib.auth.urls')),
+]
